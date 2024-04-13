b3b284d6b0ba547fa25db80567f33fdbabfe50d7
Skilltank01 <developer@skilltank.tech>
2024-04-13 21:48:32 +0530

    add game

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,131 +1,53 @@
+const choices = ['rock' , 'paper' , 'scissor'];
+const playerDisplay = document.getElementById('playerDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+const playerScoreDisplay = document.getElementById('playerScoreDisplay');
+const computerScoreDisplay = document.getElementById('computerScoreDisplay');
 
-// Complete logic of game inside this function
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-  let moves = 0;
-
-
-  // Function to 
-  const playGame = () => {
-      const rockBtn = document.querySelector('.rock');
-      const paperBtn = document.querySelector('.paper');
-      const scissorBtn = document.querySelector('.scissor');
-      const playerOptions = [rockBtn, paperBtn, scissorBtn];
-      const computerOptions = ['rock', 'paper', 'scissors']
-
-      // Function to start playing game
-      playerOptions.forEach(option => {
-          option.addEventListener('click', function () {
-
-              const movesLeft = document.querySelector('.movesleft');
-              moves++;
-              movesLeft.innerText = `Moves Left: ${10 - moves}`;
+let playerScore = 0
+let computerScore  =  0 
 
 
-              const choiceNumber = Math.floor(Math.random() * 3);
-              const computerChoice = computerOptions[choiceNumber];
+function playGame(playerChoice){
+  
+  const computerChoice = choices[Math.floor(Math.random() * 3)]
+  let result = ""
 
-              // Function to check who wins
-              winner(this.innerText, computerChoice)
-
-              // Calling gameOver function after 10 moves
-              if (moves == 10) {
-                  gameOver(playerOptions, movesLeft);
-              }
-          })
-      })
-
+  if(playerChoice === computerChoice){
+    result = "IT'S A TIE!"
+  }else{
+    switch(playerChoice){
+      case "rock":
+        result = (computerChoice === "scissor") ? "YOU WIN!":"YOU LOSE!";
+      break;
+      case "paper":
+        result = (computerChoice === "rock") ? "YOU WIN!":"YOU LOSE!";
+      break;
+      case "scissor":
+        result = (computerChoice === "paper") ? "YOU WIN!":"YOU LOSE!";
+      break;
   }
 
-  // Function to decide winner
-  const winner = (player, computer) => {
-      const result = document.querySelector('.result');
-      const playerScoreBoard = document.querySelector('.p-count');
-      const computerScoreBoard = document.querySelector('.c-count');
-      player = player.toLowerCase();
-      computer = computer.toLowerCase();
-      if (player === computer) {
-          result.textContent = 'Tie'
-      }
-      else if (player == 'rock') {
-          if (computer == 'paper') {
-              result.textContent = 'Computer Won';
-              computerScore++;
-              computerScoreBoard.textContent = computerScore;
-
-          } else {
-              result.textContent = 'Player Won'
-              playerScore++;
-              playerScoreBoard.textContent = playerScore;
-          }
-      }
-      else if (player == 'scissors') {
-          if (computer == 'rock') {
-              result.textContent = 'Computer Won';
-              computerScore++;
-              computerScoreBoard.textContent = computerScore;
-          } else {
-              result.textContent = 'Player Won';
-              playerScore++;
-              playerScoreBoard.textContent = playerScore;
-          }
-      }
-      else if (player == 'paper') {
-          if (computer == 'scissors') {
-              result.textContent = 'Computer Won';
-              computerScore++;
-              computerScoreBoard.textContent = computerScore;
-          } else {
-              result.textContent = 'Player Won';
-              playerScore++;
-              playerScoreBoard.textContent = playerScore;
-          }
-      }
-  }
-
-  // Function to run when game is over
-  const gameOver = (playerOptions, movesLeft) => {
-
-      const chooseMove = document.querySelector('.move');
-      const result = document.querySelector('.result');
-      const reloadBtn = document.querySelector('.reload');
-
-      playerOptions.forEach(option => {
-          option.style.display = 'none';
-      })
-
-
-      chooseMove.innerText = 'Game Over!!'
-      movesLeft.style.display = 'none';
-
-      if (playerScore > computerScore) {
-          result.style.fontSize = '2rem';
-          result.innerText = 'You Won The Game'
-          result.style.color = '#308D46';
-      }
-      else if (playerScore < computerScore) {
-          result.style.fontSize = '2rem';
-          result.innerText = 'You Lost The Game';
-          result.style.color = 'red';
-      }
-      else {
-          result.style.fontSize = '2rem';
-          result.innerText = 'Tie';
-          result.style.color = 'grey'
-      }
-      reloadBtn.innerText = 'Restart';
-      reloadBtn.style.display = 'flex'
-      reloadBtn.addEventListener('click', () => {
-          window.location.reload();
-      })
-  }
-
-
-  // Calling playGame function inside game
-  playGame();
-
+  
 }
+   playerDisplay.textContent=`PLAYER ${playerChoice}`;
+   computerDisplay.textContent=`Computer ${computerChoice}`; 
+   resultDisplay.textContent=result 
 
-// Calling the game function
-game();
+   resultDisplay.classList.remove("greenText" , "redText")
+
+   switch(result){
+      case "YOU WIN!":
+        resultDisplay.classList.add("greenText");
+        playerScore++
+        playerScoreDisplay.textContent = playerScore
+       break;
+       case "YOU LOSE!":
+        resultDisplay.classList.add("redText");
+        computerScore++
+        computerScoreDisplay.textContent = computerScore
+       break;
+
+   }
+}
